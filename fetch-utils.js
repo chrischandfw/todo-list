25c9310339c94fd04324ce1617b027ff0342edc9
chris_chan <chrischandfw@gmail.com>
2022-03-28 15:31:55 -0500

    added async to the rest of functions in fetch file and fixed eslint errors

--- fetch-utils.js
+++ fetch-utils.js
@@ -6,34 +6,34 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createTodo(todo) {
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
-	const response = await client
-		.from('todos')
-		.insert({
-			todo:'',
-			complete: false
-		})
+    const response = await client
+        .from('todos')
+        .insert({
+            todo:'',
+            complete: false
+        });
 
     return checkError(response);
 }
 
 export async function deleteAllTodos() {
     // delete all todos for this user in supabase
-	const response = await client
-		.from('todos')
-		.delete()
-		.match({user_id:id})
-		.single()
+    const response = await client
+        .from('todos')
+        .delete()
+        .match({ user_id:id })
+        .single();
 
     return checkError(response);
 }
 
 export async function getTodos() {
     // get all todos for this user from supabase
-	const response = await client
-		.from('todos')
-		.select('*')
-		.match({user_id:id})
-		.single()
+    const response = await client
+        .from('todos')
+        .select('*')
+        .match({ user_id:id })
+        .single();
 
 
     return checkError(response);
@@ -41,10 +41,10 @@ export async function getTodos() {
 
 export async function completeTodo(id) {
     // find the and update (set complete to true), the todo that matches the correct id
-	const response = await client
-		.from('todos')
-		.update({complete: true})
-		.match({user_id:id})
+    const response = await client
+        .from('todos')
+        .update({ complete: true })
+        .match({ user_id:id });
     return checkError(response);
 }
 
@@ -58,13 +58,13 @@ export function checkAuth() {
     if (!user) location.replace('../');
 }
 
-export function redirectIfLoggedIn() {
+export async function redirectIfLoggedIn() {
     if (await getUser()) {
         location.replace('./todos');
     }
 }
 
-export function signupUser(email, password) {
+export async function signupUser(email, password) {
     const response = await client.auth.signUp({ email, password });
 
     return response.user;
